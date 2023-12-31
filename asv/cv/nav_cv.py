46a1728545dcb0008720317df8379b80050a5dce
ChenYenRu <rebecca946487@gmail.com>
2023-12-31 19:26:08 +0800

    added template_cv, nav_cv, and testing template

--- asv/cv/nav_cv.py
+++ asv/cv/nav_cv.py
@@ -3,6 +3,8 @@ Description
 ---
 Interface for the CV class used for each mission
 
+Adapted from Team Inspiration Robosub 2023 path_cv.py
+
 """
 
 # import what you need from within the package
@@ -40,8 +42,41 @@ class CV:
         self.error_buffer = []
 
         print("[INFO] Bin CV init")
+
+    def find_red_and_green(frame):
+        """
+        Highlight red and green part of the frame
+
+        Parameters
+        ----------
+        frame
+            the frame from the camera
+        """
+        # convert the frame to HSV
+        hsv_frame = cv2.cv2Color(frame, cv2.COLOR_BGR2HSV)
+
+        # define the hsv range for red and green
+        lower_red: np.int64 = np.array([0,100,100])
+        upper_red: np.int64 = np.array([10,255,255])
+
+        lower_green: np.int64 = np.array([40,40,40])
+        upper_green: np.int64 = np.array([80,255,255])
+
+        # create masks for red and green
+        red_mask = cv2.inRange(frame, lower_red, upper_red)
+        green_mask = cv2.inRange(frame, lower_red, upper_red)
+
+        # apply the masks to the original frames
+        red_result = cv2.bitwise_and(frame, frame, mask=red_mask)
+        green_result = cv2.bitwise_and(frame, frame, mask=green_mask)
+        
+        # Combine the results to get a frame with both red and green highlighted
+        red_and_green_result = cv2.addWeighted(red_result, 1, green_result, 1, 0)
+
+        return red_and_green_result
+
     
-    def run(self, frame, target, oakd_data):
+    def run(self, frame, target, oakd_data) -> dict:
         """
         Should be all the code required to run the CV. 
         
